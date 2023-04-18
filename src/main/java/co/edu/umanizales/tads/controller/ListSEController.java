a258ded53358ca821b68183bb7a338fee2dc9a85
AlejandroSG10 <jasalazar99690@umanizales.edu.co>
2023-04-17 20:52:03 -0500

    Commit1

---
+++ src/main/java/co/edu/umanizales/tads/controller/ListSEController.java
@@ -0,0 +1,103 @@
+package co.edu.umanizales.tads.controller;
+
+
+import co.edu.umanizales.tads.controller.dto.KidDTO;
+import co.edu.umanizales.tads.controller.dto.KidsByLocationDTO;
+import co.edu.umanizales.tads.controller.dto.ResponseDTO;
+import co.edu.umanizales.tads.model.Kid;
+import co.edu.umanizales.tads.model.Location;
+import co.edu.umanizales.tads.service.ListSEService;
+import co.edu.umanizales.tads.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/listse")
+public class ListSEController {
+
+
+    @Autowired
+    private ListSEService listSEService;
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO> getKids(){
+        return new ResponseEntity<>(new ResponseDTO(
+                200,listSEService.getKids().getHead(),null), HttpStatus.OK);
+    }
+
+    @GetMapping("/invert")
+    public ResponseEntity<ResponseDTO> invert(){
+        listSEService.invert();
+        return new ResponseEntity<>(new ResponseDTO(
+                200,"Se ha invertido la lista",
+                null), HttpStatus.OK);
+
+    }
+
+    @GetMapping(path = "/orderboystostart")
+    public ResponseEntity<ResponseDTO> orderBoysToStart(){
+        listSEService.getKids().orderBoysToStart();
+        return new ResponseEntity<>(new ResponseDTO(
+                200,"Se ha ordenado la lista con los niños al comienzo",null), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/change_extremes")
+    public ResponseEntity<ResponseDTO> changeExtremes() {
+        listSEService.getKids().changeExtremes();
+        return new ResponseEntity<>(new ResponseDTO(
+                200,"Se han intercambiado los extremos",
+                null), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO> addKid(@RequestBody KidDTO kidDTO){
+        Location location = locationService.getLocationByCode(kidDTO.getCodeLocation());
+        if(location == null){
+            return new ResponseEntity<>(new ResponseDTO(
+                    404,"La ubicación no existe",
+                    null), HttpStatus.OK);
+        }
+        listSEService.getKids().add(
+                new Kid(kidDTO.getIdentification(),
+                        kidDTO.getName(), kidDTO.getAge(),
+                        kidDTO.getGender(), location));
+        return new ResponseEntity<>(new ResponseDTO(
+                200,"Se ha adicionado el petacón",
+                null), HttpStatus.OK);
+
+    }
+
+    @GetMapping(path = "/kidsbylocations")
+    public ResponseEntity<ResponseDTO> getKidsByLocation(){
+        List<KidsByLocationDTO> kidsByLocationDTOList = new ArrayList<>();
+        for(Location loc: locationService.getLocations()){
+            int count = listSEService.getKids().getCountKidsByLocationCode(loc.getCode());
+            if(count>0){
+                kidsByLocationDTOList.add(new KidsByLocationDTO(loc,count));
+            }
+        }
+        return new ResponseEntity<>(new ResponseDTO(
+                200,kidsByLocationDTOList,
+                null), HttpStatus.OK);
+    }
+    @GetMapping(path = "/kidsbydepto")
+    public ResponseEntity<ResponseDTO> getKidsByDeptoCode(){
+        List<KidsByLocationDTO> kidsByLocationDTOList1= new ArrayList<>();
+        for(Location loc: locationService.getLocations()){
+            int count = listSEService.getKids().getCountKidsByDeptoCode(loc.getCode());
+            if(count>0){
+                kidsByLocationDTOList1.add(new KidsByLocationDTO(loc,count));
+            }
+        }
+        return new ResponseEntity<>(new ResponseDTO(200,kidsByLocationDTOList1,
+                null), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/co/edu/umanizales/tads/controller/dto/RangeByAgeDTO.java
@@ -0,0 +1,19 @@
+package co.edu.umanizales.tads.controller.dto;
+
+
+import co.edu.umanizales.tads.model.Kid;
+import co.edu.umanizales.tads.model.ListSE;
+
+public class RangeByAgeDTO {
+
+
+    private byte age;
+    private int range;
+    private Kid kid;
+    private ListSE listSE;
+    int range1to3 = 0;
+    int range4to6 = 0;
+    int range7to9 = 0;
+    int range10to12 = 0;
+    int range13to15 = 0;
+}
